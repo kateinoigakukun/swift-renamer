d5a4431ae1160f4e0f41fba51fae9a5e1ab91fdf
Yuta Saito <kateinoigakukun@gmail.com>
2020-05-03 23:15:42 +0900

    Fix rewrite logic

--- Sources/SwiftRenamer/SourceRewriter.swift
+++ Sources/SwiftRenamer/SourceRewriter.swift
@@ -26,27 +26,20 @@ public class SourceRewriter {
     public func apply() -> String {
         let sorted = replacements.unique().sorted(by: {
                 $0.location.line == $1.location.line ?
-                    $0.location.column < $1.location.column :
+                    $0.location.column > $1.location.column :
                     $0.location.line < $1.location.line
         })
         var lines = content.components(separatedBy: "\n")
-        var lineOffset = 0
-        var lastLine: Int64 = 0
 
         for replacement in sorted {
-            if lastLine != replacement.location.line {
-                lineOffset = 0
-            }
+
             let lineIndex = Int(replacement.location.line) - 1
             var line = lines[lineIndex]
 
-            let startOffset = Int(replacement.location.column - 1) + lineOffset
+            let startOffset = Int(replacement.location.column - 1)
             let startIndex = line.index(line.startIndex, offsetBy: startOffset)
             let endIndex = line.index(startIndex, offsetBy: Int(replacement.length))
             line.replaceSubrange(startIndex..<endIndex, with: replacement.newText)
-            lineOffset = replacement.newText.count - Int(replacement.length)
-
-            lastLine = replacement.location.line
 
             lines[lineIndex] = line
         }
